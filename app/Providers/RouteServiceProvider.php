8f0a0537b5e08dd56054d4373c792fe9b8fe7992
amirhamada <155433521+amirmohamedis@users.noreply.github.com>
2026-06-05 21:56:29 +0300

    rout fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,10 +36,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-        });
 
-        Route::middleware('admin')
-        ->group(base_path('routes/admin.php'));
+            Route::middleware('admin')
+                ->group(base_path('routes/admin.php'));
+        });
   
     }
 
